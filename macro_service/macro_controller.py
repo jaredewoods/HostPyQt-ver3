269d533b7e8fddfb32579b93f864f09af00c1bff
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-06-16 09:17:46 -0500

    Refactor macro file loading and parsing in macro_controller
    The code for loading, reading, and parsing macro files in the macro_controller has been cleaned up and separated into smaller, more precise functions. The functions read_macro_file and parse_macro_file_content have been added for this purpose. This results in code that is easier to understand, test, and maintain.

--- macro_service/macro_controller.py
+++ macro_service/macro_controller.py
@@ -16,10 +16,26 @@ class MacroController(QObject):
         # Populate the macro ComboBox
         self.populate_macro_combobox()
 
+    @staticmethod
+    def get_macro_directory():
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        main_dir = os.path.dirname(current_dir)
+        return os.path.join(main_dir, 'resources', 'macro_sequences')
+
+    def populate_macro_combobox(self):
+        macro_directory = self.get_macro_directory()
+        macro_files = self.model.get_macro_filenames(macro_directory)
+        self.view.populate_macro_select_combo(macro_files)
+
     def on_macro_dropdown_activated(self, index):
         selected_file = self.view.macro_select_cbx.currentText()
         if selected_file:
             self.handle_macro_file_selection(selected_file)
+
+    def update_macro_ready_state(self):
+        serial_connected = self.flag_state_manager.get_flag_status('serial_connected')
+        macro_ready = serial_connected
+        self.signal_distributor.state_changed.emit('macro_ready_to_run', macro_ready, 'update')
 
     def handle_macro_file_selection(self, selected_file):
         macro_sequence_file_path = os.path.join(self.get_macro_directory(), selected_file)
@@ -27,23 +43,11 @@ class MacroController(QObject):
         self.view.macro_start_btn.setEnabled(True)
         self.update_macro_ready_state()
 
-    def update_macro_ready_state(self):
-        serial_connected = self.flag_state_manager.get_flag_status('serial_connected')
-        macro_ready = serial_connected
-        self.signal_distributor.state_changed.emit('macro_ready_to_run', macro_ready, 'update')
-
-    @staticmethod
-    def get_macro_directory():
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        main_dir = os.path.dirname(current_dir)
-        return os.path.join(main_dir, 'resources', 'macro_sequences')
-
-    def load_macro_file(self, file_name):
-        macro_directory = self.get_macro_directory()
-        file_path = os.path.join(macro_directory, file_name)
+    def read_macro_file(self, file_path):
         with open(file_path, 'r') as file:
-            lines = file.readlines()
+            return file.readlines()
 
+    def parse_macro_file_content(self, lines):
         suggested_cycles = 0
         macro_commands = []
         for line in lines:
@@ -54,10 +58,20 @@ class MacroController(QObject):
                 continue
             else:
                 macro_commands.append(line)
+        return suggested_cycles, macro_commands
 
-        # Update the UI elements
+    def update_ui_with_macro_data(self, suggested_cycles, macro_commands):
         self.view.update_total_cycles(suggested_cycles)
         self.command_view.update_macro_sequence('\n'.join(macro_commands))
+
+    def load_macro_file(self, file_name):
+        macro_directory = self.get_macro_directory()
+        file_path = os.path.join(macro_directory, file_name)
+        lines = self.read_macro_file(file_path)
+
+        suggested_cycles, macro_commands = self.parse_macro_file_content(lines)
+
+        self.update_ui_with_macro_data(suggested_cycles, macro_commands)
 
         # For testing purposes, print the parsed results
         print("Suggested Cycles:", suggested_cycles)
@@ -65,7 +79,5 @@ class MacroController(QObject):
 
         return suggested_cycles, macro_commands
 
-    def populate_macro_combobox(self):
-        macro_directory = self.get_macro_directory()
-        macro_files = self.model.get_macro_filenames(macro_directory)
-        self.view.populate_macro_select_combo(macro_files)
+
+
