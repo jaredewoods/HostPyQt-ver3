df41ba80198e03533982caf48da30df75de45166
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-06-21 16:23:56 -0500

    Replace _start_macro with start_executor
    The _start_macro method was renamed to start_executor. Additionally, the connection between macro_start_btn clicked signal and the corresponding slot has been updated. This refactor makes the code more consistent with the execution flow.

--- macro_service/macro_controller.py
+++ macro_service/macro_controller.py
@@ -22,16 +22,15 @@ class MacroController(QObject):
         self.signal_distributor = signal_distributor
         self.flag_state_manager = flag_state_manager
         self.view.macro_select_cbx.activated.connect(self.on_macro_dropdown_activated)
-        self.view.macro_start_btn.clicked.connect(self._start_macro)
+        self.view.macro_start_btn.clicked.connect(self.start_executor)
         # Connect the itemSelected signal from CommandView
         self.command_view.itemSelected.connect(self.load_macro_sequence_line)
 
         self.populate_macro_combobox()
 
-    @staticmethod
-    def _start_macro():
+    def start_executor(self):
         print("sending runMacro to executor")
-        self.start_signal.emit()
+        self.signal_distributor.state_changed.emit("macro_running", True, "update")
 
     @staticmethod
     def get_macro_directory():
