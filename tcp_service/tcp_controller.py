da0fe169aac311b32d7a000290e1a78a9081c99b
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-06-25 20:41:27 -0500

    Refactor debug message emission in TCPController
    The update removes the redundant signal `debug_message` from the `TCPController` in favor of using the `DEBUG_MESSAGE` signal from `signal_distributor`. This change makes the debug message handling more consistent and removes the unnecessary duplication of signaling mechanisms.

--- tcp_service/tcp_controller.py
+++ tcp_service/tcp_controller.py
@@ -3,8 +3,8 @@
 from PyQt6.QtCore import QObject, pyqtSignal
 
 class TCPController(QObject):
-    debug_message = pyqtSignal(str)  # Define a signal to emit log messages
-    log_message = pyqtSignal(str)  # Define a signal to emit
+    # debug_message = pyqtSignal(str)  # Define a signal to emit log messages
+    log_message = pyqtSignal(str)
 
     def __init__(self, model, view, signal_distributor):
         super().__init__()
@@ -21,17 +21,17 @@ class TCPController(QObject):
         success = self.model.connect(ip_address, port)
         if success:
             self.signal_distributor.STATE_CHANGED_SIGNAL.emit('tcp_connected', True, 'validate')
-            self.debug_message.emit(f"Connected to {ip_address}:{port}")
+            self.signal_distributor.DEBUG_MESSAGE.emit(f"Connected to {ip_address}:{port}")
             self.log_message.emit(f"Connected to {ip_address}:{port}")
         else:
             self.signal_distributor.STATE_CHANGED_SIGNAL.emit('tcp_connected', False, 'update')
-            self.debug_message.emit(f"Failed to connect to {ip_address}:{port}")
+            self.signal_distributor.DEBUG_MESSAGE.emit(f"Failed to connect to {ip_address}:{port}")
 
     def disconnect_tcp(self):
         success = self.model.disconnect()
         if success:
             self.signal_distributor.STATE_CHANGED_SIGNAL.emit('tcp_connected', False, 'update')
-            self.debug_message.emit("Disconnected from TCP connection")
+            self.signal_distributor.DEBUG_MESSAGE.emit("Disconnected from TCP connection")
 
     def update_connection_btn_state(self, connected):
         self.view.tcp_connect_btn.setEnabled(not connected)
